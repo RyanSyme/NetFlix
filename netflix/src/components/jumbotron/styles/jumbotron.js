3cabd84e087a91cf2143a665f084782d5e351e41
RyanSyme <ryu321@hotmail.com>
2021-09-07 10:03:42 +0000

    style jumbotron

--- netflix/src/components/jumbotron/styles/jumbotron.js
+++ netflix/src/components/jumbotron/styles/jumbotron.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 export const Inner = styled.div`
     display: flex;
@@ -8,23 +8,33 @@ export const Inner = styled.div`
     max-width: 1100px;
     margin: auto;
     width: 100%;
-
+    
     @media (max-width: 1000px) {
         flex-direction: column;
     }
 `;
 
-export const Item = styled.div`
-    display: flex;
+export const Pane = styled.div`
+    width: 50%;
 `;
 
-export const Container = styled.section``;
+export const Item = styled.div`
+    display: flex;
+    border-bottom: 8px solid #222;
+    padding: 50px 5%;
+    color: white;
+    overflow: hidden;
+`;
+
+export const Container = styled.section`
+    background-color: black;
+`;
 
 export const Title = styled.h1`
     font-size: 50px;
     line-height: 1.1;
     margin-bottom: 8px;
-
+    
     @media (max-width: 600px) {
         font-size: 35px;
     }
@@ -34,7 +44,7 @@ export const SubTitle = styled.h2`
     font-size: 26px;
     font-weight: normal;
     line-height: normal;
-
+    
     @media (max-width: 600px) {
         font-size: 18px;
     }
