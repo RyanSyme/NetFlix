4a9242c7f87e7f3b9448680eca9a9a74bfb0bb16
RyanSyme <ryu321@hotmail.com>
2021-09-07 09:03:09 +0000

    add style to Title and subTitle

--- netflix/src/components/jumbotron/index.js
+++ netflix/src/components/jumbotron/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container, Item, Inner } from './styles/jumbotron'
+import { Container, Item, Inner, Title, SubTitle, Image } from './styles/jumbotron'
 
 export default function Jumbotron({children, direction = 'row', ...restProps}) {
     return (
@@ -11,4 +11,16 @@ export default function Jumbotron({children, direction = 'row', ...restProps}) {
 
 Jumbotron.Container = function JumbotronContainer({ children, ...restProps }) {
     return <Container {...restProps}>{children}</Container>
+}
+
+Jumbotron.Title = function JumbotronTitle({ children, ...restProps }) {
+    return <Title {...restProps}>{children}</Title>
+}
+
+Jumbotron.SubTitle = function JumbotronSubTitle({ children, ...restProps }) {
+    return <SubTitle {...restProps}>{children}</SubTitle>
+}
+
+Jumbotron.Image = function JumbotronImage({ children, ...restProps }) {
+    return <Image {...restProps}>{children}</Image>
 }
